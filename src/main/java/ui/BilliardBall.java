28cffe8c817a9a0511ed00856893b435a0ee104f
stein202 <steinfredl5@gmail.com>
2025-06-02 16:19:00 +0200

    🥱

--- src/main/java/ui/BilliardBall.java
+++ src/main/java/ui/BilliardBall.java
@@ -2,16 +2,20 @@ package main.java.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
-import java.awt.image.BufferedImage;
+import java.awt.geom.Rectangle2D;
 
 public class BilliardBall {
     public static final int radius = 30;
     public static final double masse = 0.17;
-    public BufferedImage texture;
     public int id;
+    public double orgX, orgY;
     public double x, y;
     public double vx, vy;
+
+    public Ellipse2D ball;
+    private BilliardTable billiardTable;
 
     private BallPanel ballPanel;
     private Timer physicsTimer;
@@ -20,12 +24,18 @@ public class BilliardBall {
 
     public BilliardBall(int id, double x, double y) {
         this.id = id;
+        this.orgX = x;
+        this.orgY = y;
         this.x = x;
         this.y = y;
     }
 
     public void setBallPanel(BallPanel ballPanel) {
         this.ballPanel = ballPanel;
+    }
+
+    public void setBilliardTable(BilliardTable billiardTable) {
+        this.billiardTable = billiardTable;
     }
 
     public void drawBall(Graphics2D g2d) {
@@ -39,22 +49,15 @@ public class BilliardBall {
         else if (id == 5) g2d.setColor(Color.BLACK);
         else if (id >= 1 && id <= 7) g2d.setColor(Color.RED);
         else if (id >= 9 && id <= 15) g2d.setColor(Color.BLUE);
-
-        g2d.fill(new Ellipse2D.Double(x, y, radius, radius));
+        ball = new Ellipse2D.Double(x, y, radius, radius);
+        g2d.fill(ball);
     }
 
     public void calcImpulse(int power, double angle) {
         double v = ((2 * BilliardQueue.masse) / (BilliardBall.masse + BilliardQueue.masse) * power * 0.3);
-
         vx = v * Math.cos(Math.toRadians(angle));
         vy = v * Math.sin(Math.toRadians(angle));
-
-        System.out.println(vx);
-        System.out.println(vy);
-
-         physicsTimer = new Timer(16, e -> {
-            updatePosition();
-        });
+        physicsTimer = new Timer(16, e -> updatePosition());
         physicsTimer.start();
     }
 
@@ -62,15 +65,7 @@ public class BilliardBall {
         this.x += vx;
         this.y += vy;
 
-        if (x <= BilliardTable.tableX || x + radius >= BilliardTable.pocketRightX) {
-            vx = -vx;
-            x = Math.max(BilliardTable.tableX, Math.min(x, BilliardTable.pocketRightX - radius));
-        }
-
-        if (y <= BilliardTable.tableY || y + radius >= BilliardTable.pocketBottomY) {
-            vy = -vy;
-            y = Math.max(BilliardTable.tableY, Math.min(y, BilliardTable.pocketBottomY - radius));
-        }
+        checkCushionCollision(billiardTable.cushion);
 
         vx *= 0.98;
         vy *= 0.98;
@@ -87,5 +82,34 @@ public class BilliardBall {
 
     public boolean isBallStill() {
         return (Math.abs(vx) < 0.05 || Math.abs(vy) < 0.05);
+    }
+
+    public void checkCushionCollision(Area cushion) {
+        if (billiardTable.isInPocket(this)) {
+            vx = 0;
+            vy = 0;
+            x = orgX;
+            y = orgY;
+            physicsTimer.stop();
+            return;
+        }
+
+        if (cushion.intersects(ball.getBounds2D())) {
+            double left = billiardTable.getBounds().getMinX();
+            double right = billiardTable.getBounds().getMaxX();
+            double top = billiardTable.getBounds().getMinY();
+            double bottom = billiardTable.getBounds().getMaxY();
+
+            if (x - radius <= left || x + radius >= right) {
+                vx = -vx;
+                x += vx; // kleine Korrektur, damit der Ball nicht in der Bande hängen bleibt
+            }
+
+            // Prallen an oberer oder unterer Bande
+            if (y - radius <= top || y + radius >= bottom) {
+                vy = -vy;
+                y += vy;
+            }
+        }
     }
 }
